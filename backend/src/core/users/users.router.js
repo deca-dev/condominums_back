c8258a9d8ba461f20d13d8437ca6df9dcbd9f5a1
David Castro <davidernesto.castroanaya@gmail.com>
2023-12-20 16:18:07 -0600

    fixing user router

--- backend/src/core/users/users.router.js
+++ backend/src/core/users/users.router.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /**
  * @swagger
  * tags:
@@ -6,14 +5,11 @@
  *   description: User management
  */
 
-=======
->>>>>>> origin/main
 const usersServices = require('./users.services');
 const router = require('express').Router();
 const passport = require('passport');
 const adminValidate = require('../../middlewares/rol.middleware');
 
-<<<<<<< HEAD
 require('../../middlewares/auth.middleware')(passport);
 
 //? Main Route
@@ -185,31 +181,3 @@ router
   );
 
 module.exports = router;
-=======
-require('../../middlewares/auth.middleware')(passport)
-
-//? Main Route
-
-router.get('/',usersServices.getAllUsers); //esto es porque userServices es un objeto 
-
-//? Protected routes for each user
-router.route('/me')
-    .get(passport.authenticate('jwt', {session:false}), usersServices.getMyUser)
-    .patch(passport.authenticate('jwt', {session: false}), usersServices.patchMyUser)
-    .delete(passport.authenticate('jwt', {session: false}), usersServices.deleteMyUser)
-    
-router.route('/me/reservations')    
-    .get(passport.authenticate('jwt', {session:false}), usersServices.getMyReservations)
-
-router.route('/me/reservations/:reservation_id')
-    .patch(passport.authenticate('jwt', {session:false}), usersServices.editMyReservation)
-    .delete(passport.authenticate('jwt', {session:false}), usersServices.deleteMyReservation)
-
-router.route('/:id')
-    .get(usersServices.getUserById)
-    .patch(passport.authenticate('jwt', {session: false}),adminValidate,usersServices.patchUser)
-    .delete(passport.authenticate('jwt', {session: false}),adminValidate,usersServices.deleteUser)
-
-
-module.exports = router
->>>>>>> origin/main
